c81672d6ed6380c0d45abaa6bb7e27fd79cfd418
Fiorela <fazahuanchef@gmail.com>
2023-12-04 10:09:32 -0500

    installed graphql and resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      autoSchemaFile: join( process.cwd(), 'src/schema.gql'),
+      }),
+    ItemsModule,
+  ],
   controllers: [],
   providers: [],
 })
