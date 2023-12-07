33cd6216da94d2d25c5f93c36787a45699ad001c
Fiorela <fazahuanchef@gmail.com>
2023-12-07 10:38:27 -0500

    created new user

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { ItemsModule } from './items/items.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -29,6 +30,7 @@ import { UsersModule } from './users/users.module';
     }),
     ItemsModule,
     UsersModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,26 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserInput } from './dto/create-user.input';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UpdateUserInput } from './dto/update-user.input';
 import { User } from './entities/user.entity';
+import { SignupInput } from 'src/auth/dto/inputs/signup.input';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UsersService {
-  create(createUserInput: CreateUserInput) {
-    return 'This action adds a new user';
+
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>
+  ) {}
+
+  async create(signupInput: SignupInput): Promise<User> {
+    try{
+      const newUser = this.usersRepository.create(signupInput);
+      return await this.usersRepository.save(newUser);
+    } catch (error) {
+      console.log(error);
+      throw new BadRequestException('Algo salió mal');
+    }
   }
 
   async findAll() : Promise<User[]> {
